44270e3fba00b70e0fca7cbf037a9d10e4d49dc2
haihung01 <104615629+haihung01@users.noreply.github.com>
2023-09-11 15:19:02 +0700

    product-cart

--- src/UI/product-cart/ProductCart.jsx
+++ src/UI/product-cart/ProductCart.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Container, Row } from 'reactstrap'
 import Bg_Banner from '../bg-banner/Bg_Banner'
 
 const ProductCart = () => {
+
+    const [isChecked, setIsChecked] = useState({
+        sold: false,
+        empty: false,
+        selected: false,
+    });
+
+    // Hàm xử lý khi checkbox thay đổi trạng thái
+    const handleCheckboxChange = (name) => {
+        setIsChecked({
+            ...isChecked,
+            [name]: !isChecked[name],
+        });
+    };
+
     return (
         <>
             <section className="home-banner" >
@@ -57,12 +72,68 @@ const ProductCart = () => {
                                     </Container> */}
                                     <Container>
                                         <Row className='flex gap-5'>
-                                            <Col className='w-[700px] h-[336px] bg-slate-400 rounded-2xl'>
+                                            <Col className='w-[700px] h-[336px] border border-gray-200 rounded-2xl shadow-xl'>
+                                                <Col className=''>
+                                                    <div className='flex gap-20 mt-3 items-center ml-9'>
+                                                        <h1 className='text-2xl font-medium'>Chọn ghế</h1>
+                                                        <span className=''>Thông tin xe</span>
+                                                    </div>
+                                                    <div className='flex gap-32 items-center ml-20 mt-4'>
+                                                        <span>Tầng trên</span>
+                                                        <span>Tầng dưới</span>
+                                                    </div>
+
+                                                </Col>
+
+                                                <Col className='flex gap-5 ml-4'>
+                                                    <div className='w-[475px] '>
+                                                        <Table />
+                                                    </div>
+
+                                                    <div className=''>
+                                                        <ul className=''>
+                                                            <li className='gap-2 flex items-center'>
+                                                                <input className='w-[16px] h-[16px] bg-gray-700 ' />
+                                                                Đã bán
+                                                            </li>
+                                                            <li className='gap-2 flex items-center'>
+                                                                <input className='w-[16px] h-[16px] bg-blue-300' />
+                                                                Còn trống
+                                                            </li>
+                                                            <li className='gap-2 flex items-center'>
+                                                                <input className='w-[16px] h-[16px] bg-purple-300' />
+                                                                Đang chọn
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </Col>
                                             </Col>
 
                                             <Col>
-                                                <div className='w-[345px] h-[206px] bg-slate-400 rounded-2xl'>
-
+                                                <div className='w-[345px] h-[206px] border border-gray-200 rounded-2xl shadow-xl'>
+                                                    <h1 className='text-2xl font-medium ml-7 mt-3'>Thông tin lượt đi</h1>
+                                                    <div className='ml-7 mt-3'>
+                                                        <tr className='flex justify-between'>
+                                                            <td>Tuyến xe</td>
+                                                            <td className='mr-6'>BX Mien Tay ⇒ Da Lat</td>
+                                                        </tr>
+                                                        <tr className='flex justify-between'>
+                                                            <td>Thời gian</td>
+                                                            <td className='mr-6'>19:00 11-0r-6063</td>
+                                                        </tr>
+                                                        <tr className='flex justify-between'>
+                                                            <td>Số lượng ghế</td>
+                                                            <td className='mr-6'>1 Ghế</td>
+                                                        </tr>
+                                                        <tr className='flex justify-between'>
+                                                            <td>Số ghế</td>
+                                                            <td className='mr-6'>B06</td>
+                                                        </tr>
+                                                        <tr className='flex justify-between'>
+                                                            <td>Tổng tiền lượt đi</td>
+                                                            <td className='mr-6'>280.000đ</td>
+                                                        </tr>
+                                                    </div>
                                                 </div>
 
                                                 <div className='w-[345px] h-[171px] bg-slate-400 mt-5 rounded-2xl'>
@@ -72,7 +143,7 @@ const ProductCart = () => {
                                         </Row>
 
                                         <Row className='flex gap-5'>
-                                            <Col className='w-[700px] h-[200    px] bg-slate-400 rounded-2xl mt-[-40px]'>
+                                            <Col className='w-[700px] h-[200px] bg-slate-400 rounded-2xl mt-[-40px]'>
                                             </Col>
                                             <Col className='w-[345px] h-[100px] rounded-2xl mt-5  flex gap-10 justify-end'>
                                                 <div className='w-[112px] h-[32px] bg-slate-400 rounded-2xl '>
@@ -93,6 +164,25 @@ const ProductCart = () => {
                     </Row>
                 </Container>
             </section></>
+    )
+}
+
+function Table() {
+    return (
+        <table className="table-fixed">
+            <tbody>
+                {Array(6).fill().map((_, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {Array(6).fill().map((_, colIndex) => (
+                            <td key={colIndex} className=" border px-6 py-[6.5px] m-4">
+                                {rowIndex * 6 + colIndex + 1}
+
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     )
 }
 
