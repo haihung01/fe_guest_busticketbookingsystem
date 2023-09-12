ee92d218d7526d84dbfecb5bc24b79d6a1e278ff
haihung01 <104615629+haihung01@users.noreply.github.com>
2023-09-12 12:37:24 +0700

    done product cart

--- src/UI/product-cart/ProductCart.jsx
+++ src/UI/product-cart/ProductCart.jsx
@@ -23,59 +23,15 @@ const ProductCart = () => {
             <section className="home-banner" >
                 <Container>
                     <Row>
-                        <Col className='Widget-Section mt-20'>
-                            <div className="w-[1200px] h-[900px] p-12  rounded-2xl mx-auto bg-slate-50 shadow-2xl border border-gray-500">
+                        <Col className=' mt-20'>
+                            <div className="w-[1200px] h-[900px] p-12  rounded-2xl mx-auto bg-slate-50 border border-gray-200">
                                 <section>
-                                    {/* <Container>
-                                        <Row>
-                                            <Col className='flex align-items-center gap-28  mx-auto w-[876.2px] round-checkbox mt-3'>
-                                                <div>
-                                                    <input type="checkbox" id="myCheck1" />
-                                                    <label for="myCheck1">Một chiều</label>
-                                                </div>
-                                                <div>
-                                                    <input type="checkbox" id="myCheck2" />
-                                                    <label for="myCheck2">Khứ hồi</label>
-                                                </div>
-                                            </Col>
-
-                                            <Col className=' align-items-center  mx-auto w-[798px] h-[200px]'>
-                                                <div className='mt-3 flex  gap-4'>
-                                                    <div>
-                                                        <span className='ml-3'>Điểm đi :</span>
-                                                        <div className=' border border-gray-300 flex rounded-lg w-[252px] h-[67px]'>
-                                                            <input className='mx-auto text-center font-medium text-lg' placeholder='Chọn điểm đến'></input>
-                                                        </div>
-                                                    </div>
-                                                    <div>
-                                                        <span className='ml-3'>Điểm đến :</span>
-                                                        <div className=' border border-gray-300 flex rounded-lg w-[252px] h-[67px]'>
-                                                            <input className='mx-auto text-center font-medium text-lg' placeholder='Chọn điểm đi'></input>
-                                                        </div>
-                                                    </div>
-                                                    <div>
-                                                        <span className='ml-3'>Ngày đi :</span>
-                                                        <div className=' border border-gray-300 flex rounded-lg w-[252px] h-[67px]'>
-                                                            <input className='mx-auto text-center font-medium text-lg' placeholder='Chọn ngày '></input>
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                                <div className='mt-4'>
-                                                    <span className='ml-3'>Tìm kiếm gần đây :</span>
-                                                    <div className=' border border-gray-300 flex rounded-lg w-[252px] h-[67px]'>
-                                                        <input className='mx-auto text-center font-medium text-lg'></input>
-                                                    </div>
-                                                </div>
-                                            </Col>
-                                        </Row>
-                                    </Container> */}
                                     <Container>
-                                        <Row className='flex gap-5'>
+                                        <Row className='flex justify-between'>
                                             <Col className='w-[700px] h-[336px] border border-gray-200 rounded-2xl shadow-xl'>
                                                 <Col className=''>
                                                     <div className='flex gap-20 mt-3 items-center ml-9'>
-                                                        <h1 className='text-2xl font-medium'>Chọn ghế</h1>
+                                                        <h1 className='text-2xl font-normal'>Chọn ghế</h1>
                                                         <span className=''>Thông tin xe</span>
                                                     </div>
                                                     <div className='flex gap-32 items-center ml-20 mt-4'>
@@ -111,47 +67,73 @@ const ProductCart = () => {
 
                                             <Col>
                                                 <div className='w-[345px] h-[206px] border border-gray-200 rounded-2xl shadow-xl'>
-                                                    <h1 className='text-2xl font-medium ml-7 mt-3'>Thông tin lượt đi</h1>
+                                                    <h1 className='text-2xl font-normal ml-7 mt-3'>Thông tin lượt đi</h1>
                                                     <div className='ml-7 mt-3'>
                                                         <tr className='flex justify-between'>
-                                                            <td>Tuyến xe</td>
-                                                            <td className='mr-6'>BX Mien Tay ⇒ Da Lat</td>
+                                                            <td className='text-gray-400 font-normal'>Tuyến xe</td>
+                                                            <td className='mr-6 font-normal'>BX Mien Tay ⇒ Da Lat</td>
                                                         </tr>
                                                         <tr className='flex justify-between'>
-                                                            <td>Thời gian</td>
-                                                            <td className='mr-6'>19:00 11-0r-6063</td>
+                                                            <td className='text-gray-400 font-normal'>Thời gian</td>
+                                                            <td className='mr-6 text-green-800 font-normal'>19:00 11-0r-6063</td>
                                                         </tr>
                                                         <tr className='flex justify-between'>
-                                                            <td>Số lượng ghế</td>
-                                                            <td className='mr-6'>1 Ghế</td>
+                                                            <td className='text-gray-400 font-normal'>Số lượng ghế</td>
+                                                            <td className='mr-6 font-normal'>1 Ghế</td>
                                                         </tr>
                                                         <tr className='flex justify-between'>
-                                                            <td>Số ghế</td>
-                                                            <td className='mr-6'>B06</td>
+                                                            <td className='text-gray-400 font-normal'>Số ghế</td>
+                                                            <td className='mr-6 text-green-800 font-normal'>B06</td>
                                                         </tr>
                                                         <tr className='flex justify-between'>
-                                                            <td>Tổng tiền lượt đi</td>
-                                                            <td className='mr-6'>280.000đ</td>
+                                                            <td className='text-gray-400 font-normal'>Tổng tiền lượt đi</td>
+                                                            <td className='mr-6 text-green-800 font-normal'>280.000đ</td>
                                                         </tr>
                                                     </div>
                                                 </div>
 
-                                                <div className='w-[345px] h-[171px] bg-slate-400 mt-5 rounded-2xl'>
+                                                <div className='w-[345px] h-[171px] mt-5 border border-gray-200 rounded-2xl shadow-xl'>
+                                                    <h1 className='text-2xl font-normal ml-7 mt-3'>Chi tiết giá</h1>
+                                                    <div className='ml-7 mt-3'>
+                                                        <tr className='flex justify-between'>
+                                                            <td className='text-gray-400 font-normal'>Giá vé lượt đi</td>
+                                                            <td className='mr-6 font-normal text-orange-600'>280.000đ</td>
+                                                        </tr>
+                                                        <tr className='flex justify-between'>
+                                                            <td className='text-gray-400 font-normal'>Phí thanh toán</td>
+                                                            <td className='mr-6 text-green-800 font-normal'>0đ</td>
+                                                        </tr>
+                                                        <hr className='mr-6 mt-3' />
+                                                        <tr className='flex justify-between mt-3'>
+                                                            <td className='text-gray-400 font-normal'>Tổng tiền</td>
+                                                            <td className='mr-6 font-normal text-orange-600'>280.000đ</td>
+                                                        </tr>
 
+                                                    </div>
                                                 </div>
                                             </Col>
                                         </Row>
 
                                         <Row className='flex gap-5'>
-                                            <Col className='w-[700px] h-[200px] bg-slate-400 rounded-2xl mt-[-40px]'>
+                                            <Col className='w-[700px] h-[210px] border border-gray-200 rounded-2xl shadow-xl mt-[-40px] '>
+                                                <h1 className='ml-7 text-xl font-normal mt-3 text-orange-500'>ĐIỀU KHOẢN & LƯU Ý</h1>
+                                                <div className='ml-7 mt-1'>
+                                                    <span className="mr-5 ">
+                                                        (*) Quý khách vui lòng có mặt tại bến xuất phát của xe trước ít nhất 30 phút giờ xe khởi hành, mang theo thông báo đã thanh toán vé thành công có chứa mã vé được gửi từ hệ thống FUTA BUS LINE. Vui lòng liên hệ Trung tâm tổng đài <span class="text-orange-500">1900 6067</span>  để được hỗ trợ.
+                                                    </span>
+                                                    <span className="flex mt-3 mr-5">
+                                                        (*) Nếu quý khách có nhu cầu trung chuyển, vui lòng liên hệ Tổng đài trung chuyển trước khi đặt vé. Chúng tôi không đón/trung chuyển tại những điểm xe trung chuyển không thể tới được.
+                                                    </span>
+
+                                                </div>
                                             </Col>
-                                            <Col className='w-[345px] h-[100px] rounded-2xl mt-5  flex gap-10 justify-end'>
-                                                <div className='w-[112px] h-[32px] bg-slate-400 rounded-2xl '>
-
-                                                </div>
-                                                <div className='w-[112px] h-[32px] bg-slate-400 rounded-2xl '>
-
-                                                </div>
+                                            <Col className='w-[345px] h-[100px] rounded-2xl mt-5  flex gap-10 justify-end ml-8'>
+                                                <button className='w-[112px] h-[32px] border border-gray-300 rounded-2xl flex justify-center text-center items-center text-orange-500'>
+                                                    Hủy
+                                                </button>
+                                                <button className='w-[112px] h-[32px] bg-orange-500 rounded-2xl flex justify-center text-center items-center text-gray-50'>
+                                                    Thanh toán
+                                                </button>
                                             </Col>
 
                                         </Row>
