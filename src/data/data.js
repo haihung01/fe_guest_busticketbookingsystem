24a72906d3ea974b59853bb31541a2a880fddc35
haihung01 <104615629+haihung01@users.noreply.github.com>
2023-10-03 13:04:06 +0700

    done schedule

--- src/data/data.js
+++ src/data/data.js
@@ -2,52 +2,269 @@
 export const schedule = [
     {
         id: 1,
+        routes: '1. Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 2,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 3,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+    },
+    {
+        id: 4,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+    },
+    {
+        id: 5,
         address: 'Bình Dương, Hồ Chí Minh',
         route: 'Tuyến bus số 618: Bến xe miền Tây HCM – Khu du lịch Đại Nam, Bình Dương',
+        img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
+
+    },
+    {
+        id: 6,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 7,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 8,
+        routes: '1. Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 9,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 10,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+    },
+    {
+        id: 11,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+    },
+    {
+        id: 12,
+        address: 'Bình Dương, Hồ Chí Minh',
+        route: 'Tuyến bus số 618: Bến xe miền Tây HCM – Khu du lịch Đại Nam, Bình Dương',
+        img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
+
+    },
+    {
+        id: 13,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 14,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 15,
+        routes: '1. Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 16,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 17,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+    },
+    {
+        id: 18,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+    },
+    {
+        id: 19,
+        address: 'Bình Dương, Hồ Chí Minh',
+        route: 'Tuyến bus số 618: Bến xe miền Tây HCM – Khu du lịch Đại Nam, Bình Dương',
+        img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
+
+    },
+    {
+        id: 20,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 7,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 1,
+        routes: '1. Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 2,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 3,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+    },
+    {
+        id: 4,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+    },
+    {
+        id: 5,
+        address: 'Bình Dương, Hồ Chí Minh',
+        route: 'Tuyến bus số 618: Bến xe miền Tây HCM – Khu du lịch Đại Nam, Bình Dương',
+        img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
+
+    },
+    {
+        id: 6,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+    {
+        id: 7,
+        routes: 'Tuyến số 01: Thủ Dầu Một – Mỹ Phước',
+        route: 'BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.',
+        time: '5h30 – 19h00',
+        distance: '15 phút/chuyến'
+
+    },
+]
+
+export const other = [
+    {
+        id: 1,
+        address: 'Tuyến bus số 02:Thủ Dầu Một – Bầu Bàng',
+        route: ' BX Khách Tỉnh Bình Dương, Bình Dương, Bình Dương',
         img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
 
     },
     {
         id: 2,
-        address: 'Bình Dương, Hồ Chí Minh',
-        route: 'Tuyến bus số 618: Bến xe miền Tây HCM – Khu du lịch Đại Nam, Bình Dương',
-        img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
-
-    },
-    {
-        id: 3,
-        address: 'Bình Dương, Hồ Chí Minh',
-        route: 'Tuyến bus số 618: Bến xe miền Tây HCM – Khu du lịch Đại Nam, Bình Dương',
-        img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
-
-    },
-    {
-        id: 4,
-        address: 'Bình Dương, Hồ Chí Minh',
-        route: 'Tuyến bus số 618: Bến xe miền Tây HCM – Khu du lịch Đại Nam, Bình Dương',
-        img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
-
-    },
-    {
-        id: 5,
-        address: 'Bình Dương, Hồ Chí Minh',
-        route: 'Tuyến bus số 618: Bến xe miền Tây HCM – Khu du lịch Đại Nam, Bình Dương',
-        img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
-
-    },
-    {
-        id: 5,
-        address: 'Bình Dương, Hồ Chí Minh',
-        route: 'Tuyến bus số 618: Bến xe miền Tây HCM – Khu du lịch Đại Nam, Bình Dương',
-        img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
-
-    },
-    {
-        id: 5,
-        address: 'Bình Dương, Hồ Chí Minh',
+        address: 'Tuyến bus số 01: Thủ Dầu Một - Mỹ Phước',
         route: 'Tuyến bus số 618: Bến xe miền Tây HCM – Khu du lịch Đại Nam, Bình Dương',
         img: 'https://media.diadiem247.com/uploads/w150/uploads/xe-bus-binh-duong.jpg'
 
     },
 ]
 
+export const location = [
+    {
+        id: 1,
+        address: 'Lii’s Homestay - Homestay tại Hà Nội',
+        route: '  9C Ngách 12/2 Đặng Thai Mai, Phường Quảng An, Quận Tây Hồ, Hà Nội',
+        img: 'https://media.diadiem247.com/uploads/w150/2023/09/24/IMG_1099.png'
+
+    },
+    {
+        id: 2,
+        address: 'Nhà Của Bu - Homestay tại Hà Nội',
+        route: ' 2A Lý Nam Đế, Phường Hàng Mã, Quận Hoàn Kiếm, Hà Nội',
+        img: 'https://media.diadiem247.com/uploads/w150/2023/09/24/IMG_1095.jpeg'
+
+    },
+    {
+        id: 3,
+        address: 'Harmony Homestay - Homestay tại Hà Nội',
+        route: '  36 Phất Lộc, Phường Hàng Buồm, Quận Hoàn Kiếm, Hà Nội',
+        img: 'https://media.diadiem247.com/uploads/w150/2023/09/24/IMG_1091.jpeg'
+
+    },
+    {
+        id: 4,
+        address: 'Momento Homestay - Homestay tại Hà Nội',
+        route: '  48A Tràng Thi, Phường Hàng Bông, Quận Hoàn Kiếm, Hà Nội',
+        img: 'https://media.diadiem247.com/uploads/w150/2023/09/24/IMG_1088.png'
+
+    },
+    {
+        id: 5,
+        address: 'The Chi Novel - Homestay tại Hà Nội',
+        route: '  49 Nhà Chung, Phường Hàng Trống, Quận Hoàn Kiếm, Hà Nội',
+        img: 'https://media.diadiem247.com/uploads/w150/2023/09/24/IMG_1084.jpeg'
+
+    },
+]
--- src/pages/Schedule2.jsx
+++ src/pages/Schedule2.jsx
@@ -1,35 +1,47 @@
 import React from 'react'
 import { Col, Container, Row } from 'reactstrap';
 import { schedule } from '../data/data';
+import { other } from '../data/data';
 
-const Schedule2 = ({ address, route, img }) => {
+
+const Schedule2 = ({ routes, route, time, disance }) => {
     return (
         <>
             <section className="home-banner" >
                 <Container>
                     <Row>
-                        <Col className=' '>
-                            <div className="w-[1200px] h-[1050px] p-12  rounded-2xl mx-auto bg-slate-50 border border-gray-200 flex gap-3">
-                                <Col className='border border-gray-900 w-[1000px] h-[950px]'>
-                                    {schedule.map((item, index) => (
-                                        <div key={index} className='flex gap-10 mt-7'>
-                                            <img className='h-[97px] w-[97px] rounded-full' src={item.img} />
-                                            <div className=''>
-                                                <h1 className='font-medium text-lg'>{item.address}</h1>
-                                                <hr className='mt-2 border-t-2 border-gray-300' />
-                                                <Col className='flex gap-1 mt-2'>
-                                                    <img src='https://www.svgrepo.com/show/418950/address-location-map.svg' className='w-[17px] h-[23px]' />
-                                                    <span className='text-gray-400'>{item.route}</span>
-                                                </Col>
-                                            </div>
+                        <Col className=''>
+                            <h1 className='text-4xl text-center d-flex p-12'>Lộ trình các tuyến xe bus tại Bình Dương</h1>
+                            <div className="w-[1200px] h-[4650px] p-12  rounded-2xl mx-auto bg-slate-50 border border-gray-200  gap-3 ">
+                                <Row>
+                                    <span className='font-medium'>Nhằm hỗ trợ hành khách dễ dàng tra cứu các tuyến xe buýt đang hoạt động tại Bình Dương, dưới đây là một số thông tin giới thiệu lộ trình xe bus tại Bình Dương.</span>
+                                    <img
+                                        className='rounded-lg mx-auto w-[1100px] h-[650px] mt-7'
+                                        src='https://dulichbinhduong.org.vn/storage/travels/90f6ueNJL7ZK6axPbhWaRHCwDPr7KxwPoAfOtizW.jpg' />
+                                </Row>
 
-                                        </div>
+                                <Row className='mt-5'>
+                                    {schedule.map((item, index) => (
+                                        <Col key={index}>
+                                            <span className='font-medium'>1. Tuyến số 01: Thủ Dầu Một – Mỹ Phước</span>
+                                            <div className='ml-10 p-4'>
+                                                <ul style={{ listStyleType: 'disc', paddingLeft: '0' }}>
+                                                    <li style={{ textAlign: 'center' }}>
+                                                        <span className='font-medium'>Lộ trình :</span> BX Khách Tỉnh Bình Dương – CMT8 – Cầu Ông Đành – Chợ Cây Dừa – Mũi Tàu – QL13 – Sở Sao – Mỹ Phước.
+                                                    </li>
+                                                    <li style={{ textAlign: 'center' }}>Thời gian : <span className='font-medium'>5h30 – 19h00</span></li>
+                                                    <li style={{ textAlign: 'center' }}>Giãn cách : <span className='font-medium'>15 phút/chuyến</span></li>
+                                                </ul>
+                                            </div>
+                                            <hr className='mt-2' />
+                                        </Col>
+
                                     ))}
 
-                                </Col>
+                                </Row>
 
-                                <Col className='border border-gray-900 w-[350px] h-[500px]'>
-                                </Col>
+
+
                             </div>
                         </Col>
 
@@ -76,3 +88,20 @@ const Schedule2 = ({ address, route, img }) => {
 }
 
 export default Schedule2;
+
+{/* <Col className='border border-gray-900 w-[740px] h-[950px]'>
+{schedule.map((item, index) => (
+    <div key={index} className='flex gap-10 mt-7'>
+        <img className='h-[97px] w-[97px] rounded-full' src={item.img} />
+        <div className=''>
+            <h1 className='font-medium text-lg'>{item.address}</h1>
+            <hr className='mt-2 border-t-2 border-gray-300' />
+            <Col className='flex gap-1 mt-2'>
+                <img src='https://www.svgrepo.com/show/418950/address-location-map.svg' className='w-[17px] h-[23px]' />
+                <span className='text-gray-400'>{item.route}</span>
+            </Col>
+        </div>
+    </div>
+))}
+
+</Col> */}
