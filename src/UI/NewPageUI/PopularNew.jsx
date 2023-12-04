231aab0c6f7e4327a76a6e2ad01dc67c92f401cd
Hoang <hoangdmse150785@fpt.edu.vn>
2023-12-04 07:56:33 +0700

    update New

--- src/UI/NewPageUI/PopularNew.jsx
+++ src/UI/NewPageUI/PopularNew.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./PopularNew.scss";
 import dataPopNew from "../../components/data/dataPopNew";
 import CardPopular_new from "./CardPopular_new";
 import CardNewsNewPost from "./CardNewsNewPost";
 const PopularNew = () => {
+  const [news, setNews] = useState([]); // Store provinces data
+
   const splitDataIntoRows = (dataPopNew, rowSize) => {
     const rows = [];
     for (let i = 0; i < dataPopNew.length; i += rowSize) {
@@ -13,12 +15,30 @@ const PopularNew = () => {
     return rows;
   };
 
-  const cardRows = splitDataIntoRows(dataPopNew, 2);
+  const cardRows = splitDataIntoRows(news, 2);
+  useEffect(() => {
+    fetch("http://btbs.ap-southeast-1.elasticbeanstalk.com/news")
+      .then((response) => response.json())
+      .then((data) => {
+        const mappedData = data.data.map((item) => ({
+          id: item.idNews.toString(),
+          name: item.title, // Ensure the property name matches the actual key in the response
+          detail : item.description,
+          img: item.listImg,
+          createdDate: item.createdDate,
+          updatedDate: item.updatedDate
+        }));
+        setNews(mappedData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []); // Ensure the dependencies are properly managed
 
   return (
     <>
       <div className="PopularNewContainer">
-        <div className="wrap_New_pop">
+        {/* <div className="wrap_New_pop">
           <span className="Title_new_pop"> Bài viết phổ biến </span>
           <div className="card-list">
             {dataPopNew.map((card) => (
@@ -33,7 +53,7 @@ const PopularNew = () => {
               />
             ))}
           </div>
-        </div>
+        </div> */}
         <div className="wrap_New_NewPost">
           <span className="Title_new_pop"> Bài viết mới nhất </span>
           <div className="card-list-Newpost">
@@ -42,12 +62,10 @@ const PopularNew = () => {
                 {row.map((card) => (
                   <CardNewsNewPost
                     key={card.id}
-                    imageSrc={card.imageSrc}
-                    title={card.title}
+                    imageSrc={card.img}
+                    title={card.name}
                     detail={card.detail}
-                    authorAvatar={card.authorAvatar}
-                    authorName={card.authorName}
-                    date={card.date}
+                    date={card.updatedDate}
                   />
                 ))}
               </div>
